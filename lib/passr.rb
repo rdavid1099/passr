7b84dfaf085042442156fc52fb3b68d7725a500d
Ryan Workman <rdavid1099@gmail.com>
2017-05-03 15:48:25 -0600

    Base generating and decrypting/ revealing functionality implemented

--- lib/passr.rb
+++ lib/passr.rb
@@ -1,5 +1,36 @@
-require "passr/version"
+require 'passr/version'
+require 'passr/encryptor'
+require 'passr/generator'
 
+# Overall Passr functionality contained within module
 module Passr
-  # Your code goes here...
+  # Generate an encrypted password
+  #
+  # @param [Hash] options options for password generating
+  # @option options [Integer] :length length of characters of generated password
+  # @option options [String] :nonce nonce to encrypt password using known nonce
+  # @return [Hash] data regarding the encrypted generated password
+  def self.generate(**options)
+    encryptor = Passr::Encryptor.new(options[:nonce])
+    generated_password = Passr::Generator.password(options[:length])
+    encrypted_password = encryptor.encrypt(generated_password)
+    {nonce: encryptor.nonce,
+     password: generated_password,
+     encrypted_password: encrypted_password}
+  end
+
+  # Decrypt and reveal encrypted password
+  #
+  # @param [Hash] params params of necessary information for decryption
+  # @option params [String] :password encrypted password returned from Passr.generate
+  # @option params [String] :nonce nonce returned from Passr.generate
+  # @return [String] decrypted password
+  def self.reveal(**params)
+    begin
+      encryptor = Passr::Encryptor.new(params[:nonce])
+      encryptor.decrypt(params[:password])
+    rescue => e
+      false
+    end
+  end
 end
--- test/passr_test.rb
+++ test/passr_test.rb
@@ -4,4 +4,26 @@ class PassrTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Passr::VERSION
   end
+
+  def test_it_generates_encrypted_passwords
+    data = Passr.generate length: 20
+
+    assert_equal 20, data[:password].length
+    assert data[:password] != data[:encrypted_password]
+    assert data[:nonce]
+  end
+
+  def test_it_returns_decrypted_password_with_correct_data
+    data = Passr.generate length: 20
+    decrypted = Passr.reveal password: data[:encrypted_password], nonce: data[:nonce]
+
+    assert_equal data[:password], decrypted
+  end
+
+  def test_it_returns_false_if_wrong_info_used_for_reveal
+    data = Passr.generate length: 20
+
+    refute Passr.reveal password: data[:encrypted_password], nonce: 1
+    refute Passr.reveal password: 'nope', nonce: data[:nonce]    
+  end
 end
